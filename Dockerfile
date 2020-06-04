61c5caf740963e00ab05f9c0f7200a5c0b2a097f
Olivier Berger <olivier.berger@telecom-sudparis.eu>
2020-06-04 17:56:24 +0200

    Poted for CentOS 7

--- Dockerfile
+++ Dockerfile
@@ -1,32 +1,75 @@
-FROM node:stretch
-LABEL Lawrence Stubbs <technoexpressnet@gmail.com>
+FROM centos:centos7 as base
 
-RUN apt-get update && apt-get -y install mongodb sudo
+# LABEL Lawrence Stubbs <technoexpressnet@gmail.com>
 
-COPY startmeshcentral.sh /
-RUN adduser --quiet meshserver \
-    && mkdir -p /home/meshserver/meshcentral-data \
-    && chmod +x /startmeshcentral.sh \
-	&& mv -f /startmeshcentral.sh /home/meshserver/meshcentral-data/startmeshcentral.sh
+# Environment variables
+ENV \
+    LC_ALL=en_US.UTF-8                \
+    RUNTIME_DEPENDENCIES="            \
+        wget"                         \
+    BUILD_DEPENDENCIES="              \
+        git"
+
+# RUN apt-get update && apt-get -y install mongodb sudo
+# Bring environment up-to-date and install guacamole-server dependencies
+
+    # yum -y install epel-release          && \
+    # yum -y install $RUNTIME_DEPENDENCIES && \
+RUN yum -y update                        && \
+    yum clean all
+
+RUN curl -sL https://rpm.nodesource.com/setup_10.x | bash -
+RUN yum install -y nodejs
+
+RUN useradd meshserver && \
+    mkdir -p /home/meshserver/meshcentral-data
+
+FROM base as builder
+
+RUN yum -y install $BUILD_DEPENDENCIES
+
+#RUN git clone --progress https://github.com/Ylianst/MeshCentral.git
+
+FROM base as runtime
+
+RUN yum -y update                        && \
+    yum -y install $RUNTIME_DEPENDENCIES && \
+    yum clean all
 
 COPY package.json /home/meshserver/
+#COPY --from=builder MeshCentral/package.json /home/meshserver/
+
 COPY config.json /home/meshserver/meshcentral-data/
+
+#RUN env
+
+#RUN npm install meshcentral --loglevel verbose
+
 RUN su - meshserver \
 	&& cd /home/meshserver \
-    && npm install meshcentral 
-    
+    && npm install meshcentral --loglevel verbose
+
+COPY startmeshcentral.sh /home/meshserver/meshcentral-data/startmeshcentral.sh
+RUN chmod +x /home/meshserver/meshcentral-data/startmeshcentral.sh
+
+RUN chown -R meshserver:meshserver /home/meshserver/meshcentral-data
+
 ENV PORT 443  
 ENV REDIRPORT 80  
-ENV MPSPORT 4443
-ENV EMAIL mail@host
-ENV HOST host.ltd
-ENV SMTP smtp.host.ltd
-ENV USER smtp@user
-ENV PASS smtppass!
-ENV DB netdb
-ENV MONGODB "mongodb://127.0.0.1:27017/meshcentral"
-ENV MONGODBCOL "meshcentral"
+# ENV MPSPORT 4443
+#ENV EMAIL mail@host
+# ENV HOST host.ltd
+# ENV SMTP smtp.host.ltd
+# ENV USER smtp@user
+# ENV PASS smtppass!
+# ENV DB netdb
+# ENV MONGODB "mongodb://127.0.0.1:27017/meshcentral"
+# ENV MONGODBCOL "meshcentral"
 
-EXPOSE 25 80 443 4443 27017 27018
+EXPOSE 25 80 443
+# EXPOSE 25 80 443 4443 27017 27018
+
+#USER meshserver
 
 ENTRYPOINT ["/home/meshserver/meshcentral-data/startmeshcentral.sh"]
+#CMD [ "/home/meshserver/meshcentral-data/startmeshcentral.sh" ]
--- startmeshcentral.sh
+++ startmeshcentral.sh
@@ -1,68 +1,27 @@
 #!/bin/sh
+
+#set -x
+
+echo "staring mes central"
+
 export PORT  
 export REDIRPORT
-export MPSPORT
+#export MPSPORT
 export EMAIL
-export HOST
-export SMTP
-export USER
-export PASS
-export DB
-export MONGODB
-export MONGODBCOL
+#export HOST
+#export SMTP
+#export USER
+#export PASS
+#export DB
+#export MONGODB
+#export MONGODBCOL
 
 su - meshserver
 cd /home/meshserver/
-npm install meshcentral
+# npm install meshcentral
 
 sed -i "s#: 443,#: $PORT,#" meshcentral-data/config.json
 sed -i "s#: 80,#: $REDIRPORT,#" meshcentral-data/config.json
-sed -i "s#: 4443,#: $MPSPORT,#" meshcentral-data/config.json
-
-if [ "$EMAIL" != 'mail@host' ]
-then
-    sed -i "s#\"email\": \"mail@host\",#\"email\": \"$EMAIL\",#" meshcentral-data/config.json
-    sed -i "s#\"from\": \"mail@host\",#\"from\": \"$EMAIL\",#" meshcentral-data/config.json
-else
-    sed -i "s#\"email\": \"mail@host\",#\"email\": \"support@$HOSTNAME\",#" meshcentral-data/config.json
-    sed -i "s#\"from\": \"mail@host\",#\"from\": \"support@$HOSTNAME\",#" meshcentral-data/config.json
-fi
-
-if [ "$HOST" != 'host.ltd' ]
-then
-    sed -i "s#\"names\": \"host.ltd\",#\"names\": \"$HOST\",#" meshcentral-data/config.json
-else
-    sed -i "s#\"names\": \"host.ltd\",#\"names\": \"$HOSTNAME\",#" meshcentral-data/config.json
-fi
- 
-if [ "$SMTP" != 'smtp.host.ltd' ]
-then
-    sed -i "s#\"host\": \"smtp.host.ltd\",#\"host\": \"$SMTP\",#" meshcentral-data/config.json
-else
-    sed -i "s#\"host\": \"smtp.host.ltd\",#\"host\": \"$HOSTNAME\",#" meshcentral-data/config.json    
-fi
-
-if [ "$USER" != 'smtp@user' ] || [ "$PASS" != 'smtppass!' ]
-then
-    sed -i "s#\"user\": \"smtp@user\",#\"user\": \"$USER\",#" meshcentral-data/config.json
-    sed -i "s#\"pass\": \"smtppass!\",#\"pass\": \"$PASS\",#" meshcentral-data/config.json
-else
-    sed -i "s#\"user\": \"smtp@user\",#\"user\": \"\",#" meshcentral-data/config.json
-    sed -i "s#\"pass\": \"smtppass!\",#\"pass\": \"\",#" meshcentral-data/config.json  
-fi
-
-if [ "$DB" != "netdb" ] && ! [ -f mongodbready ]
-then
-	sed -i "s#\"settings\": {#\"settings\": {\n\t\"MongoDb\": \"$MONGODB\",\n\t\"MongoDbCol\": \"$MONGODBCOL\",#" meshcentral-data/config.json   
-	node meshcentral --dbexport
-	node meshcentral --mongodb mongodb://127.0.0.1:27017/meshcentral --dbimport
-	touch mongodbready
-fi
-
-if [ "$DB" != "netdb" ]
-then
-    service mongod start
-fi
 
 if [ -f "/etc/letsencrypt/archive/$HOSTNAME/cert1.pem" ]
 then
@@ -76,9 +35,11 @@ then
     ln -sf "/etc/letsencrypt/archive/$HOSTNAME/cert1.pem" meshcentral-data/mpsserver-cert-public.crt
 fi
 
-if ! [ -f meshcentral-data/agentserver-cert-private.key ]
-then 
-	node node_modules/meshcentral/meshcentral.js --cert $HOSTNAME
-else 
-	node node_modules/meshcentral/meshcentral.js
-fi
+# if ! [ -f meshcentral-data/agentserver-cert-private.key ]
+# then 
+# 	node node_modules/meshcentral/meshcentral.js --cert $HOSTNAME
+# else 
+# 	node node_modules/meshcentral/meshcentral.js
+# fi
+node node_modules/meshcentral/meshcentral.js
+
